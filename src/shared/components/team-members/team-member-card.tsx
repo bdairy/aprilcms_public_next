194bba573f29e47e5f8cae46387e86690f0d45ee
Bdair <bdairy@hotmail.com>
2023-08-05 16:04:37 +0300

    team members

--- src/shared/components/team-members/team-member-card.tsx
+++ src/shared/components/team-members/team-member-card.tsx
@@ -5,10 +5,12 @@ import Image from 'next/image';
 export default function TeamMemberCard(params: { member: ITeamMember; locale: string }) {
   const { member, locale } = params;
   return <div className="team-member-card">
-
-    <Image className="image" src={member.image} width={300} height={300} alt={member.name} />
+<div className="content">
     <h2 className='name'>{member.name}</h2>
     <p className="position">{member.position}</p>
+    </div>
+    <Image className="image" src={member.image} width={300} height={300} alt={member.name} />
+
 
   </div>;
 }
